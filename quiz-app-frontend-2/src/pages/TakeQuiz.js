0dafefa04bdff197b4f3bb64d4388536cfe4f7f8
Divine Namirembe <131985298+divinenamirembe@users.noreply.github.com>
2025-02-20 18:41:18 +0300

    Update TakeQuiz.js

--- quiz-app-frontend-2/src/pages/TakeQuiz.js
+++ quiz-app-frontend-2/src/pages/TakeQuiz.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './TakeQuiz.css'
+import './takeQuiz.css'
 
 const TakeQuiz = () => {
   const [categories, setCategories] = useState([]);
